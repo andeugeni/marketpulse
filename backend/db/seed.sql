3670d27bfc3f5d8fae2698da62b22886194034b6
andeugeni <andeugeni@gmail.com>
2026-03-22 11:28:51 -0700

    changed seed to import other important degen stonks

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,4 +1,6 @@
 INSERT INTO tickers (symbol, name) VALUES
     ('RDDT', 'Reddit Inc.'),
-    ('RKLB', 'Rocket Lab Corp.')
+    ('RKLB', 'Rocket Lab Corp.'),
+    ('GOOG', 'Alphabet Inc.'),
+    ('SMCI', 'Supermmicro Computer Inc.')
 ON CONFLICT DO NOTHING;
